29ccc074a35b48b8d06e36be2ce7c716ed687aa3
jamesvrooney <jamesvrooney@hotmail.com>
2022-03-04 18:02:14 +0000

    Initial commit.

---
+++ src/main/java/com/jamesvrooney/SpringSecurityInActionApplication.java
@@ -0,0 +1,13 @@
+package com.jamesvrooney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityInActionApplication.class, args);
+	}
+
+}
