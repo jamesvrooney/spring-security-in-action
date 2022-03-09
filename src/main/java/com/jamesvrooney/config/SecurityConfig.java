07c268d5eb43f492631668a429290839ceab1fdb
jamesvrooney <jamesvrooney@hotmail.com>
2022-03-09 18:18:14 +0000

    Finish example 02

--- src/main/java/com/jamesvrooney/config/SecurityConfig.java
+++ src/main/java/com/jamesvrooney/config/SecurityConfig.java
@@ -1,9 +1,15 @@
 package com.jamesvrooney.config;
 
 import com.jamesvrooney.filters.CsrfTokenLogger;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CsrfFilter;
 
 @Configuration
@@ -12,9 +18,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
 
-        http.addFilterAfter(new CsrfTokenLogger(), CsrfFilter.class)
-                .authorizeRequests()
+        http.authorizeRequests()
                 .anyRequest()
-                .permitAll();
+                .authenticated();
+
+        http.formLogin()
+                .defaultSuccessUrl("/main", true);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var userDetailsService = new InMemoryUserDetailsManager();
+
+        var james = User.withUsername("james")
+                .password("password")
+                .authorities("READ")
+                .build();
+
+        userDetailsService.createUser(james);
+
+        return userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
