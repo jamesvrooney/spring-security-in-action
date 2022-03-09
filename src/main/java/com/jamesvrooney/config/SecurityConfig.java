a1f482f50140aad404d18a72897e98b625ee6375
jamesvrooney <jamesvrooney@hotmail.com>
2022-03-09 23:59:04 +0000

    Finished example 3

--- src/main/java/com/jamesvrooney/config/SecurityConfig.java
+++ src/main/java/com/jamesvrooney/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.jamesvrooney.config;
 
-import com.jamesvrooney.filters.CsrfTokenLogger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,7 +9,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CsrfFilter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -18,12 +16,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
 
+        http.csrf(c -> {
+           c.ignoringAntMatchers("/ciao");
+        });
+
         http.authorizeRequests()
                 .anyRequest()
-                .authenticated();
-
-        http.formLogin()
-                .defaultSuccessUrl("/main", true);
+                .permitAll();
     }
 
     @Bean
---
+++ src/main/java/com/jamesvrooney/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package com.jamesvrooney.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @PostMapping("/hello")
+    public String postHello() {
+        return "Post Hello!";
+    }
+
+    @PostMapping("/ciao")
+    public String postCiao() {
+        return "Post Ciao";
+    }
+}
